8d27cdf7898cb9c83d11b396036f10d39fda79da
Xeyanix <jan.goralewski@o2.pl>
2023-03-13 19:53:48 +0100

    start working on components 1

--- konfigurator/src/App.js
+++ konfigurator/src/App.js
@@ -1,16 +1,14 @@
 import React from 'react';
-import { useState } from "react";
+// import { useState } from "react";
 import AddProducts from './components/AddProducts/AddProducts';
 import ProductList from './components/ProductList/ProductList';
 import SelectedComponents from './components/SelectedComponents/SelectedComponents';
 import Filters from './components/Filters/ProductsFilters';
-import Motherboards from './common/consts/motherboard';
+// import Motherboards from './common/consts/motherboard';
 import styles from './App.module.scss';
 
 function App() {
-  const [products, setProducts] = useState(Motherboards);
-  const [shoppingList, setShoppingList] = useState([]);
-  const [productsToDisplay, setProductsToDisplay] = useState(products);
+ 
 
 
   return (
