83ecead01c14e87cab24b45a2168e3ac08f07154
Jan Góralewski <jan.goralewski@o2.pl>
2024-02-15 23:34:45 -0500

    before set API

---
+++ konfigurator/API/server.js
@@ -0,0 +1,26 @@
+const { urlencoded } = require('express');
+const express = require('express');
+const res = require('express/lib/response');
+const bodyParser = require('body-parser');
+
+const app = express();
+cors = require("cors");
+
+app.use(cors());
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: true }));
+
+const logger = (req, res, next) => {
+    console.log('URL: ', req.originalUrl);
+    next();
+}
+
+app.set('view engine', 'ejs');
+app.use(logger);
+
+const productsRoutes = require('./routes/products');
+
+app.use('/products', productsRoutes)
+
+app.listen(9000);
